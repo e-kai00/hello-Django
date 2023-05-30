43893aee9dc3891c16062e899bc5c335e44234d2
Katerina <yelkaterina@gmail.com>
2023-05-30 12:35:17 +0200

    Run coverage, add tests to tests_models and tests_views

--- todo/tests_models.py
+++ todo/tests_models.py
@@ -7,3 +7,10 @@ class TestModels(TestCase):
     def test_done_defaults_to_false(self):
         item = Item.objects.create(name='Test todo item')
         self.assertFalse(item.done)
+
+
+    def test_item_str_method_returns_name(self):
+        item = Item.objects.create(name='Test todo item')
+        self.assertEqual(str(item), 'Test todo item')
+                         
+    
